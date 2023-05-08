544a8398e2cfd12c8f01b8decf39194fcb2368e9
Axel Valiente <axelvalientexd@gmail.com>
2023-05-08 14:53:04 -0300

    api endpoints changed

--- src/controllers/getCharDetail.js
+++ src/controllers/getCharDetail.js
@@ -4,9 +4,7 @@ const { URL_BASE, API_KEY } = process.env;
 
 const getCharDetail = async (req, res) => {
   try {
-    const response = await axios.get(
-      `${URL_BASE}/character/${req.params.id}?key=${API_KEY}`
-    );
+    const response = await axios.get(`${URL_BASE}/character/${req.params.id}`);
     const data = response.data;
     const { id, image, name, gender, species, origin, status, location } = data;
     console.log(data);
